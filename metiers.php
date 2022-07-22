7d9b3352f7d3a94f1f1fd374963ea2e8db8dab77
AnasLabzar <77747369+AnasLabzar@users.noreply.github.com>
2022-07-22 19:43:35 +0100

    0.13

--- metiers.php
+++ metiers.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Template Mo">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Template Mo">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
-    <title>Education - List of Meetings</title>
+  <title>Education - List of Meetings</title>
 
 
-    <?php include 'layouts/head.php'; ?>
-  </head>
+  <?php include 'layouts/head.php'; ?>
+</head>
 
 <body>
 
-   
+
 
   <?php include 'layouts/header.php'; ?>
 
@@ -25,24 +25,146 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <h6>Here are our upcoming meetings</h6>
+          <h6>no métiers</h6>
           <h2>Upcoming Meetings</h2>
         </div>
       </div>
     </div>
   </section>
 
-  <section class="meetings-page" id="meetings">
-    <div class="container">
+  <section class="metieres-" id="meetings">
+    <section class="section-deratisation">
+      <div class="container">
+        <div class="row gap-5">
+          <div class="col">
+            <h1>Dératisation</h1>
+            <div class="wpb_wrapper">
+              <div class="vc_custom_heading vc_custom_1489149916557 text_align_left has_icon">
+                <h2 style="font-size: 22px;">Comptez sur AJS pour la dératisation de votre bureau, magasin ou maison au Maroc</h2>
+              </div>
+              <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrappeer">
+                  <ul>
+                    <li>Appareils électroménagers, en rongeant les câbles électriques ils peuvent engendrer un court circuit parfois fatale, tel qu’un incendie ou une explosion si un conduit de gaz est affecté.</li>
+                    <li>Tuyauteries et canalisations, en causant des fuites d’eau souvent difficiles à localiser et réparer si elles sont encastrées.</li>
+                    <li>Boiseries et meubles en bois sont facilement dégradés</li>
+                    <li>Matériaux tels que les isolants (laine de verre, ouate) et autres matières friables</li>
+                    <li>Tissus, vêtements et tapis</li>
+                    <li>Nourriture et denrées alimentaires peuvent être consommées par les rats, mais surtout deviennent inutilisables du fait de leur souillure par les excréments des rongeurs</li>
+                  </ul>
+                  <h2 style="font-size: 22px;" align="justify"><strong>Nos solutions de dératisation</strong></h2>
+                  <h3 style="font-size: 18px;" align="justify">+ Les appâts AntiCoagulant, Rodonticide, Mort au rat, Raticide, Souricide</h3>
+                  <h3 style="font-size: 18px;" align="justify">+ Le piégeage des souris et des rats</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <img style="position: relative; top: 3em;" src="assets/images/deratisation-01.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
 
-    </div>
-  </section>
 
-  <?php include 'layouts/footer.php'; ?>
+    <section class="section-desinsectisation">
+      <div class="container">
+        <div class="row gap-5">
+          <div class="col">
+            <img style="position: relative; top: 1em;" src="assets/images/deratisation-02.jpg" alt="">
+          </div>
+          <div class="col">
+            <h1>Désinsectisation</h1>
+            <div class="wpb_wrapper">
+              <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                  <h2><b>Désinsectisation réalisée par des professionnels au Maroc pour éliminer les nuisibles</b></h2>
+                  <p><span style="font-weight: 400;">&nbsp;A la maison ou en entreprise, les insectes représentent une grande partie des parasites au Maroc qui peuvent nuire à votre santé, et vous empêchent de vivre dans un environnement sain. Pouvant être la cause de plusieurs maladies infectieuses, les insectes détériorent les matériaux mais également les aliments (commerces, maisons, résidences, restaurants…).&nbsp;</span></p>
+                  <p><span style="font-weight: 400;">Pour chaque type d’insectes une désinsectisation spéciale.</span></p>
+                  <p><span style="font-weight: 400;">4 grandes familles d’insectes nuisibles sont répertoriées au Maroc&nbsp;:&nbsp;&nbsp;</span></p>
+                  <ul>
+                    <li style="font-weight: 400;"><b>Les insectes rampants</b><span style="font-weight: 400;"> qui ont besoin d’une surface pour se déplacer, comme les cafards, les fourmis, les punaises, les araignées, les scorpions mais aussi les vers.&nbsp;</span></li>
+                    <li style="font-weight: 400;"><b>Les insectes volants</b><span style="font-weight: 400;"> qui sont souvent autour des espaces verts.</span></li>
+                    <li style="font-weight: 400;"><b>Les insectes relatifs au bois</b><span style="font-weight: 400;"> comme les termites et la grosse vrillette du bois.&nbsp;</span></li>
+                    <li style="font-weight: 400;"><b>Les insectes de passages</b><span style="font-weight: 400;"> qui peuvent causer dans certains cas des dégâts majeurs aux plantes et à certains types de matériaux.</span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <section class="section-deratisation">
+      <div class="container">
+        <div class="row gap-5">
+          <div class="col">
+            <h1>Dératisation</h1>
+            <div class="wpb_wrapper">
+              <div class="vc_custom_heading vc_custom_1489149916557 text_align_left has_icon">
+                <h2 style="font-size: 22px;">Comptez sur AJS pour la dératisation de votre bureau, magasin ou maison au Maroc</h2>
+              </div>
+              <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrappeer">
+                  <ul>
+                    <li>Appareils électroménagers, en rongeant les câbles électriques ils peuvent engendrer un court circuit parfois fatale, tel qu’un incendie ou une explosion si un conduit de gaz est affecté.</li>
+                    <li>Tuyauteries et canalisations, en causant des fuites d’eau souvent difficiles à localiser et réparer si elles sont encastrées.</li>
+                    <li>Boiseries et meubles en bois sont facilement dégradés</li>
+                    <li>Matériaux tels que les isolants (laine de verre, ouate) et autres matières friables</li>
+                    <li>Tissus, vêtements et tapis</li>
+                    <li>Nourriture et denrées alimentaires peuvent être consommées par les rats, mais surtout deviennent inutilisables du fait de leur souillure par les excréments des rongeurs</li>
+                  </ul>
+                  <h2 style="font-size: 22px;" align="justify"><strong>Nos solutions de dératisation</strong></h2>
+                  <h3 style="font-size: 18px;" align="justify">+ Les appâts AntiCoagulant, Rodonticide, Mort au rat, Raticide, Souricide</h3>
+                  <h3 style="font-size: 18px;" align="justify">+ Le piégeage des souris et des rats</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <img style="position: relative; top: 3em;" src="assets/images/deratisation-01.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
 
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
+    <section class="section-desinsectisation">
+      <div class="container">
+        <div class="row gap-5">
+          <div class="col">
+            <img style="position: relative; top: 1em;" src="assets/images/deratisation-02.jpg" alt="">
+          </div>
+          <div class="col">
+            <h1>Désinsectisation</h1>
+            <div class="wpb_wrapper">
+              <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                  <h2><b>Désinsectisation réalisée par des professionnels au Maroc pour éliminer les nuisibles</b></h2>
+                  <p><span style="font-weight: 400;">&nbsp;A la maison ou en entreprise, les insectes représentent une grande partie des parasites au Maroc qui peuvent nuire à votre santé, et vous empêchent de vivre dans un environnement sain. Pouvant être la cause de plusieurs maladies infectieuses, les insectes détériorent les matériaux mais également les aliments (commerces, maisons, résidences, restaurants…).&nbsp;</span></p>
+                  <p><span style="font-weight: 400;">Pour chaque type d’insectes une désinsectisation spéciale.</span></p>
+                  <p><span style="font-weight: 400;">4 grandes familles d’insectes nuisibles sont répertoriées au Maroc&nbsp;:&nbsp;&nbsp;</span></p>
+                  <ul>
+                    <li style="font-weight: 400;"><b>Les insectes rampants</b><span style="font-weight: 400;"> qui ont besoin d’une surface pour se déplacer, comme les cafards, les fourmis, les punaises, les araignées, les scorpions mais aussi les vers.&nbsp;</span></li>
+                    <li style="font-weight: 400;"><b>Les insectes volants</b><span style="font-weight: 400;"> qui sont souvent autour des espaces verts.</span></li>
+                    <li style="font-weight: 400;"><b>Les insectes relatifs au bois</b><span style="font-weight: 400;"> comme les termites et la grosse vrillette du bois.&nbsp;</span></li>
+                    <li style="font-weight: 400;"><b>Les insectes de passages</b><span style="font-weight: 400;"> qui peuvent causer dans certains cas des dégâts majeurs aux plantes et à certains types de matériaux.</span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include 'layouts/footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
@@ -55,54 +177,55 @@
     <script src="assets/js/slick-slider.js"></script>
     <script src="assets/js/custom.js"></script>
     <script>
-        //according to loftblog tut
-        $('.nav li:first').addClass('active');
+      //according to loftblog tut
+      $('.nav li:first').addClass('active');
 
-        var showSection = function showSection(section, isAnimate) {
-          var
+      var showSection = function showSection(section, isAnimate) {
+        var
           direction = section.replace(/#/, ''),
           reqSection = $('.section').filter('[data-section="' + direction + '"]'),
           reqSectionPos = reqSection.offset().top - 0;
 
-          if (isAnimate) {
-            $('body, html').animate({
-              scrollTop: reqSectionPos },
+        if (isAnimate) {
+          $('body, html').animate({
+              scrollTop: reqSectionPos
+            },
             800);
-          } else {
-            $('body, html').scrollTop(reqSectionPos);
-          }
+        } else {
+          $('body, html').scrollTop(reqSectionPos);
+        }
 
-        };
+      };
 
-        var checkSection = function checkSection() {
-          $('.section').each(function () {
-            var
+      var checkSection = function checkSection() {
+        $('.section').each(function() {
+          var
             $this = $(this),
             topEdge = $this.offset().top - 80,
             bottomEdge = topEdge + $this.height(),
             wScroll = $(window).scrollTop();
-            if (topEdge < wScroll && bottomEdge > wScroll) {
-              var
+          if (topEdge < wScroll && bottomEdge > wScroll) {
+            var
               currentId = $this.data('section'),
               reqLink = $('a').filter('[href*=\\#' + currentId + ']');
-              reqLink.closest('li').addClass('active').
-              siblings().removeClass('active');
-            }
-          });
-        };
-
-        $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
-          e.preventDefault();
-          showSection($(this).attr('href'), true);
+            reqLink.closest('li').addClass('active').
+            siblings().removeClass('active');
+          }
         });
+      };
 
-        $(window).scroll(function () {
-          checkSection();
-        });
+      $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function(e) {
+        e.preventDefault();
+        showSection($(this).attr('href'), true);
+      });
+
+      $(window).scroll(function() {
+        checkSection();
+      });
     </script>
 </body>
 
 
-  </body>
+</body>
 
 </html>
